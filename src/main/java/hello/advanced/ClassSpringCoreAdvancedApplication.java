4228d16125b44381986a2d2e7d77ff6cf3df21e3
mybabygrand <amuro_ray@kakao.com>
2021-11-10 21:37:27 +0900

    init.

---
+++ src/main/java/hello/advanced/ClassSpringCoreAdvancedApplication.java
@@ -0,0 +1,13 @@
+package hello.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassSpringCoreAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassSpringCoreAdvancedApplication.class, args);
+    }
+
+}
